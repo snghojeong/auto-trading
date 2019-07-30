a245eab70714c33d034855cd181c7131d91f369d
snghojeong <snghojeong@gmail.com>
2019-07-30 11:26:15 +0900

    Implement to move all traders in traders.py

--- traders.py
+++ traders.py
@@ -1,3 +1,26 @@
+class SimpleTrader:
+    def __init__(self, purchase_amount):
+        self.max_amount = 0
+        self.total_amount = 0
+        self.purchase_amount = purchase_amount
+        self.avg_price = 0
+        self.income = 0
+
+    def deal(self, curr_price):
+        if self.total_amount == 0:
+            self.total_amount = self.purchase_amount / curr_price
+            self.avg_price = curr_price
+        elif self.avg_price < curr_price:
+            self.income = self.income + (self.total_amount*curr_price - self.total_amount*self.avg_price)
+            self.total_amount = 0
+        else:
+            curr_amount = self.purchase_amount / curr_price
+            total_price = (self.total_amount*self.avg_price) + (curr_amount*curr_price)
+            if total_price > self.max_amount:
+                self.max_amount = total_price
+            self.total_amount = self.total_amount + curr_amount
+            self.avg_price = total_price / self.total_amount
+
 class Trader:
     def __init__(self, purchase_amount):
         self.max_amount = 0
