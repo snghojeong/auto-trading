a308520a8dee91ab652b8c8e5818c2b83b9026ba
snghojeong <snghojeong@gmail.com>
2020-09-06 20:18:15 +0900

    Modify simlator to apply logic

--- sim.py
+++ sim.py
@@ -1,4 +1,19 @@
 from pandas_datareader import data
+from matplotlib import pyplot as plt
+import math
 
-snp = data.DataReader('^GSPC', 'yahoo')
-print(snp)
+kospi = data.DataReader('^KS11', 'yahoo')
+# kosdaq = data.DataReader('KQ11', 'yahoo')
+
+cash = 10000000
+asset = 0
+tracking = []
+for oprice, cprice in zip(kospi['Open'], kospi['Close']):
+    cash = cash + (asset * oprice)
+    tracking.append(cash)
+    asset = math.floor(cash / cprice)
+    cash = cash - (asset * cprice)
+
+plt.subplot(1,1,1)
+plt.plot(tracking, label='Stock ratio')
+plt.show()
