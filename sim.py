579f46a03685c1c28774af4d62cefd9e35f48dab
snghojeong <snghojeong@gmail.com>
2020-09-07 17:13:47 +0900

    Modify simulator to plot with key

--- sim.py
+++ sim.py
@@ -1,19 +1,25 @@
 from pandas_datareader import data
 from matplotlib import pyplot as plt
 import math
+from pandas.plotting import register_matplotlib_converters
 
-kospi = data.DataReader('^KS11', 'yahoo')
+register_matplotlib_converters()
+
+kospi = data.DataReader('^KS11', 'yahoo', start='1900-01-01')
 # kosdaq = data.DataReader('KQ11', 'yahoo')
 
 cash = 10000000
 asset = 0
-tracking = []
-for oprice, cprice in zip(kospi['Open'], kospi['Close']):
-    cash = cash + (asset * oprice)
+tracking = list() 
+print(kospi['Close'])
+for key in kospi['Open'].keys():
+    cash = cash + (asset * kospi['Open'][key])
     tracking.append(cash)
-    asset = math.floor(cash / cprice)
-    cash = cash - (asset * cprice)
+    asset = math.floor(cash / kospi['Close'][key])
+    cash = cash - (asset * kospi['Close'][key])
 
-plt.subplot(1,1,1)
-plt.plot(tracking, label='Stock ratio')
+plt.subplot(2,1,1)
+plt.plot(tracking, label='1')
+plt.subplot(2,1,2)
+plt.plot(tracking, label='2')
 plt.show()
